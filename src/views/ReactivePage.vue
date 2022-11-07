4479cb25133e33412eddd2eb67affe3cd0b8c61b
mahend <hendartea@gmail.com>
2022-11-07 16:13:48 +0700

    update ref vs reactive

--- src/views/ReactivePage.vue
+++ src/views/ReactivePage.vue
@@ -11,21 +11,65 @@
     </ul>
   </nav>
 
-  <div>
-   <h1> This is Reactive Segment Page learning </h1>
+  <div class="wrap">
+
+    <div class="reactive">
+
+      <h1> This is Reactive Segment </h1>
+
+      {{state.count}}
+
+      <button @click="tambah">+</button>
+    </div>
+
+    <hr>
+
+    <div class="ref">
+      <h1>This ref segment</h1>
+      {{hitung.tugas}}
+      <button @click="tambahya">+</button>
+
+    </div>
+
   </div>
 </template>
 
-<style scoped>
-  .nameclass {
-    color: blue;
+<script setup>
+  import {reactive, ref} from 'vue';
+
+  const state = reactive({count: 0})
+
+  const tambah = () => {
+    state.count++;
   }
 
-  .counter {
+  //ref pakai objek
+  const hitung = {
+    tugas: ref(80),
+    uts: ref(80),
+    uas: ref(90)
+  }
+
+  const tambahya = () => {
+    hitung.tugas.value++
+  }
+
+</script>
+
+<style scoped>
+  .wrap {
     margin-left: 50px;
   }
 
-  nav{
+  .reactive {
+    padding-bottom: 10px;
+  }
+
+  .ref {
+    padding-bottom: 10px;
+  }
+
+  nav {
     margin: 0;
     padding: 0;
     text-align: right;
